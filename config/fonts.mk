83595cf54702e362e0275d5d0daab60e8f239bac
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-15 16:03:53 +0530

    vendor: fonts: Add Google Sans font and change output dir
    Change-Id: I7fd353cb2991caa1cb9b7b5dfed86225db44ef86
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ config/fonts.mk
@@ -0,0 +1,9 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/wings/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/wings/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
+    vendor/wings/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/wings/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/wings/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
+    vendor/wings/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
+    vendor/wings/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
