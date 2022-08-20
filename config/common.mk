ecc92d2063f69106d5a63140e5bf44cdfa2019d2
Karan Parashar <whyredfire@gmail.com>
2022-08-20 04:34:14 +0200

    vendor: bootanimation: Import from Pixel
    Change-Id: I6951a103b5ad4c0ba86b1989bd0ae4c121e580e6

--- config/common.mk
+++ config/common.mk
@@ -102,6 +102,9 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Bootanimation
+include vendor/wings/config/bootanimation.mk
+
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper
