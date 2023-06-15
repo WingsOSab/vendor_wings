7c9c9d58712611379e01f70efc69a67dc44b69df
aswin7469 <aswinas@pixysos.com>
2023-06-15 18:38:50 +0530

    fonts: Update from TQ3A.230605.012
    Change-Id: I4386110d5a29bf8d3625a60f004bc99f2f755047
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -3,4 +3,5 @@ PRODUCT_COPY_FILES += \
     vendor/wings/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/wings/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/wings/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/wings/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
     vendor/wings/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
