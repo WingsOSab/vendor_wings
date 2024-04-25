PixelOS-AOSP/vendor_aosp

common: Conditionally build Updater on official builds
Change-Id: I821e377c36e1426134e1c68fb23824afb3402e43

config/common.mk
@@ -160,13 +160,6 @@ endif
 # Bootanimation
 include vendor/aosp/config/bootanimation.mk
 
-# PixelOS packages
-PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc \
-
 # Charger
 PRODUCT_PACKAGES += \
     product_charger_res_images
@@ -274,6 +267,9 @@ include vendor/aosp/config/version.mk
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
+
+# OTA
+$(call inherit-product, vendor/aosp/config/ota.mk)
 
 # Pixel Framework
 $(call inherit-product, vendor/pixel-framework/config.mk)

config/ota.mk
@@ -1,0 +1,8 @@
+# Updater
+ifeq ($(IS_OFFICIAL),true)
+    PRODUCT_PACKAGES += \
+        Updater
+
+    PRODUCT_COPY_FILES += \
+        vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+endif

WingsOSab/vendor_wings

config/common.mk
@@ -85,7 +85,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_PACKAGES += \
     build-manifest
 
-# PixelOS packages
+# WingsOS packages
 PRODUCT_PACKAGES += \
     Updater
 
@@ -192,5 +192,8 @@ CUSTOM_LOCALES += \
     fur_IT
 
 include vendor/wings/config/version.mk
+
+# OTA
+$(call inherit-product, vendor/wings/config/ota.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

config/ota.mk
@@ -1,0 +1,8 @@
+# Updater
+ifeq ($(IS_OFFICIAL),true)
+    PRODUCT_PACKAGES += \
+        Updater
+
+    PRODUCT_COPY_FILES += \
+        vendor/wings/prebuilt/common/etc/init/init.wings-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.wings-updater.rc
+endif