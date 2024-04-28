OdSazib/android_vendor_awaken-1

vendor: Add build finished info
Change-Id: I6b7a45705bb20280ed71a8226fa0d57530c28747
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

build/tasks/bacon.mk
@@ -20,8 +20,18 @@ AWAKEN_TARGET_PACKAGE := $(PRODUCT_OUT)/Awaken-$(AWAKEN_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AWAKEN_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(AWAKEN_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(AWAKEN_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(AWAKEN_TARGET_PACKAGE)" >&2
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(AWAKEN_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"MD5: "${CL_RED}" `cat $(AWAKEN_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}

WingsOSab/vendor_wings

Change-Id: I464fd2540005599681e8a896d95f35721f61145e
Co-authored-by: xAbdaziz <i@abdaziz.dev>
Signed-off-by: janak <niraulajanak2019@gmail.com>

build/tasks/bacon.mk
@@ -19,8 +19,18 @@
 WINGS_TARGET_PACKAGE := $(PRODUCT_OUT)/Wings-$(WINGS_VERSION).zip
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(WINGS_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(WINGS_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(WINGS_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(WINGS_TARGET_PACKAGE)" >&2
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(WINGS_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(WINGS_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(WINGS_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(WINGS_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}